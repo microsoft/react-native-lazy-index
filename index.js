ef4960718faf129055705c9eefee35fb6a720dd0
Tommy Nguyen <tonguye@microsoft.com>
2019-10-30 14:44:36 +0100

    Initial version

---
+++ index.js
@@ -0,0 +1,4 @@
+const BatchedBridge = require("react-native/Libraries/BatchedBridge/BatchedBridge");
+BatchedBridge.registerCallableModule("ReactExperienceLoader", {
+  load: (...names) => names.forEach(codegen.require("inlineRequireModules"))
+});
---
+++ inlineRequireModules.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+const fs = require("fs");
+const { experiences } = JSON.parse(
+  fs.readFileSync(
+    require.resolve("./package.json", {
+      paths: module.parent && module.parent.paths
+    }),
+    "utf8"
+  )
+);
+
+module.exports = `name => {
+  switch (name) {
+    ${Object.keys(experiences)
+      .map(name => `case "${name}": return require("${experiences[name]}");`)
+      .join("")}
+  }
+}`;
