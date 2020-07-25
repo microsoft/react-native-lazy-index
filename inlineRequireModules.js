33b336cd340fe6a96bba42851614b73dc7cc1042
Tommy Nguyen <tn0502@gmail.com>
2020-07-26 01:41:41 +0200

    Bump Babel to 7.10, and Prettier to 2.0

--- inlineRequireModules.js
+++ inlineRequireModules.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const { experiences } = JSON.parse(
   fs.readFileSync(
     require.resolve("./package.json", {
-      paths: [".", ...(module.parent ? module.parent.paths : [])]
+      paths: [".", ...(module.parent ? module.parent.paths : [])],
     }),
     "utf8"
   )
@@ -17,7 +17,7 @@ if (!experiences) {
 module.exports = `name => {
   switch (name) {
     ${Object.keys(experiences)
-      .map(name => `case "${name}": return require("${experiences[name]}");`)
+      .map((name) => `case "${name}": return require("${experiences[name]}");`)
       .join("")}
   }
 }`;
